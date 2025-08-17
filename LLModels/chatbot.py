ed3a484a030cd4bcd13da15395fb0630894cdfbf
Deepa Nandhini R <deepanandhini.r@siemens-healthineers.com>
2025-08-17 22:29:20 +0530

    Learning models

---
+++ LLModels/chatbot.py
@@ -0,0 +1,8 @@
+# from langchain_community.llms import Ollama
+import streamlit as st
+
+
+# llm=Ollama(model="phi:latest",base_url="http://ollama-container:11434",verbose=True)
+
+st.title("Chat with Ollama")
+st.write("If you see this, Streamlit works.")
